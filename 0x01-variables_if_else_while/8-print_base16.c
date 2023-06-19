b60100587c38f9f9c9c29754827ab5a19ea6a404
Kokui9 <ianastasia132@gmail.com>
2023-06-19 12:16:08 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (True)
+ *
+ */
+int main(void)
+{
+int b;
+char a;
+for (base = '0'; b <= '9'; b++)
+putchar(b);
+for (a = 'a'; a <= 'f'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
