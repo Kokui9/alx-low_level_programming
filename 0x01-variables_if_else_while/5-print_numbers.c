d05fda32696b754f016c89f1ebec4872b65d1d96
Kokui9 <ianastasia132@gmail.com>
2023-06-19 11:36:22 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -9,9 +7,9 @@
  */
 int main(void)
 {
-char letter;
-for(letter= 'a'; letter <= 'z'; letter++)
-putchar(letter);
-putchar('\n');
+int num;
+for (num = 0; num <= 9; num++)
+printf("%d", num);
+printf("\n");
 return (0);
 }
