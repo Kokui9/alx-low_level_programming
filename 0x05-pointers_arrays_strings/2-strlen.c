658dc23cc66c0c57f1d3392606b37e92ee6aca21
Kokui9 <ianastasia132@gmail.com>
2023-06-30 08:47:12 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
 /**
-  *int__strlen - Returns the length of a string.
-  * @str: The string to get the length of 
-  * Return: The length of @str.
-  */
-int  size_t_strlen(const char *str)
-
+ * _strlen - a function that returns the length of a string.
+ * @str: The string to get the length
+ * Return: - nothing
+ */
+size_t _strlen(const char *str)
 {
-    size_t length = 0;
-
-    while (*str++)
-    length++;    
-    return (length);
+size_t length = 0;
+while (*str++)
+{
+length++;
+}
+return (length);
 }
