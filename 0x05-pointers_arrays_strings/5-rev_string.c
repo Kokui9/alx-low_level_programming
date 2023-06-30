fa163f959a00564a9c256984de68d3439964baf7
Kokui9 <ianastasia132@gmail.com>
2023-06-30 09:17:13 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,8 +6,8 @@
  */
 void rev_string(char *s)
 {
-        int len = 0, index = 0;
-        char temp;
+int len = 0, index = 0;
+char temp;
 while (s[index++])
 len++;
 for (index = len - 1; index >= len / 2; index--)
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * puts2 - prints other characters of string
+ * @str: the string to test
+ * Return: void
+ */
+void puts2(char *str)
+{
+        int i, j = 0;
+while (str[j] != '\0')
+{
+j++;
+}
+for (i = 0; i < j; i += 2)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
